b0675bf1cc53150a08814fae7e41f4e39a596dcb
iTchTheRightSpot <emmanueluluabuike@gmail.com>
2025-09-28 03:42:57 -0400

    final push for the night

--- java-service/src/main/java/com/itchtherightspot/kafka/Application.java
+++ java-service/src/main/java/com/itchtherightspot/kafka/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaApplication {
+class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KafkaApplication.class, args);
+    public static void main(final String... args) {
+        SpringApplication.run(Application.class, args);
     }
 
 }
